ed5559799cf6dc9b0f3bd6f00adf9bb58db001f1
Vít Novotný <witiko@mail.muni.cz>
2018-06-08 23:38:21 +0200

    Add setup.py

---
+++ mias_search/__init__.py
@@ -0,0 +1,10 @@
+"""
+The NTCIR Math MIaS Reranking package reranks MIaS results using density, and
+probability estimates extracted from datasets in the NTCIR-11 Math-2, and
+NTCIR-12 MathIR XHTML5 format.
+"""
+
+
+__author__ = "Vit Novotny"
+__version__ = "0.1.0"
+__license__ = "MIT"
---
+++ setup.py
@@ -0,0 +1,50 @@
+from os import path
+from setuptools import setup, find_packages
+
+AUTHOR = "Vit Novotny"
+HERE = path.abspath(path.dirname(__file__))
+SOURCE_URL = "https://github.com/MIR-MU/mias-search"
+
+# Get the long description from the README file
+with open(path.join(HERE, "README.md"), encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+
+setup(
+    author=AUTHOR,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Text Processing :: Markup :: XML",
+    ],
+    description="""
+        The MIaS Search package implements the Math Information Retrival system
+        that won the NTCIR-11 Math-2 main task (Růžička et al., 2014).
+    """,
+    entry_points={
+        'console_scripts': [
+            'mias-search=mias_search.__main__:main',
+        ],
+    },
+    keywords="ntcir mias math_information_retrieval",
+    install_requires=[
+        "tqdm ~= 4.23.3",
+    ],
+    license="MIT",
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    maintainer=AUTHOR,
+    name="mias_search",
+    packages=find_packages(),
+    python_requires="~= 3.4",
+    project_urls={
+        "Source": SOURCE_URL,
+    },
+    url=SOURCE_URL,
+    version="0.1.0",
+)

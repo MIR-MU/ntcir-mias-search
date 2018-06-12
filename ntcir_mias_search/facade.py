6611e6a6958c2e5ce2916e0af3af90c54a48719d
Vít Novotný <witiko@mail.muni.cz>
2018-06-12 20:06:14 +0200

    Fix missing space in a message

--- ntcir_mias_search/facade.py
+++ ntcir_mias_search/facade.py
@@ -171,7 +171,7 @@ def rerank_and_merge_results(
                 if len(result_list) < num_results:
                     if topic not in already_warned:
                         LOGGER.warning(
-                            "Result list for topic %s contains only %d / %d results, sampling"
+                            "Result list for topic %s contains only %d / %d results, sampling "
                             "the dataset", topic, len(result_list), num_results)
                         already_warned.add(topic)
                     result_list.extend(artificial_results[:num_results - len(result_list)])

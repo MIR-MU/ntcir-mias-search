1827a6fff3eab21a8e494d166030deb49252f139
Vít Novotný <witiko@mail.muni.cz>
2018-06-24 22:31:53 +0200

    Cherry-pick 0.2.1 (Add plotting capabilities), and remove UML diagrams

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         "tqdm ~= 4.23.3",
         "lxml ~= 4.2.1",
+        "matplotlib ~= 2.2.2",
         "numpy ~= 1.14.3",
         "requests ~= 2.18.4",
     ],
@@ -49,5 +50,5 @@ setup(
         "Source": SOURCE_URL,
     },
     url=SOURCE_URL,
-    version="0.2.0",
+    version="0.21",
 )
